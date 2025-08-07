5e28001f6e225eb07c64d9ad84dd0d51388c26f0
barsha20061001 <barshadgp212@gmail.com>
2025-08-07 20:41:18 +0530

    ...

--- weather/src/components/weathercard.jsx
+++ weather/src/components/weathercard.jsx
@@ -3,13 +3,22 @@ export default function Weathercard({ weather }) {
 
   const { name, main, weather: weatherInfo } = weather;
 
+
+
+
+
+
   return (
+
+   
+  
     <div className="bg-blue-100 rounded-lg p-4 shadow mt-4 text-left">
       <h2 className="text-2xl font-semibold mb-2">{name}</h2>
       <p className="text-xl">{weatherInfo[0].main}</p>
       <p className="text-5xl font-bold">{main.temp}°C</p>
       <p className="text-sm mt-2">Feels like: {main.feels_like}°C</p>
       <p className="text-sm">Humidity: {main.humidity}%</p>
+      <p className="text-sm">Wind Speed: {weather.wind.speed} m/s</p>
     </div>
   );
 }
